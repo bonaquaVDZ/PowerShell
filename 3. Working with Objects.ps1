113132ea82d5db2283d216c31dd4a059cb20804b
VDZ <70633897+bonaquaVDZ@users.noreply.github.com>
2022-02-09 11:23:34 +0100

    Adding Files

---
+++ 3. Working with Objects.ps1
@@ -0,0 +1,178 @@
+﻿# 10. Objects
+# 11. Sorting
+# 12. Filtering Using the Where Cmdlet
+# 13. Filtering Using Loops
+# 14. Foreach Loop
+# 15. Arrays
+# 16. Hash Tables
+
+
+##########################################################################
+########################## 10. Objects ###################################
+##########################################################################
+
+dir | Get-Member
+$files = dir
+$files
+$files | gm
+$files[1]
+$files[0..5]
+$files[1, 5]
+$files[0..3] | Get-Member
+$files[0..2].name
+$files[0..2].fullname
+$files[0..2].LastAccessTime
+$files[0..2].LastWriteTimeUtc
+$files[0..2].LastWriteTime
+$files[0..2].length
+
+### LEARNING
+# UTC - Universal Time Coordinated
+# CET - Central European Time (Poland, Germany) (UTC +01.00)
+# EET - Eastern European Time (Ukraine, Latvia) (UTC +02.00)
+# Moscow Time/ Turkey Time (Russia, Belarus, Turkey) (UTC +03.00)
+### LEARNING
+
+
+##########################################################################
+########################## 11. Sorting ###################################
+##########################################################################
+
+
+Get-Help sort
+
+dir
+dir | sort
+ls | sort -descending
+ls | sort -Property Name
+ls | sort -Property LastWriteTime
+ls | sort -Property LastAccessTime
+
+$files | sort -Property Length
+
+
+
+
+##########################################################################
+###################### 12. Filtering Using the Where Cmdlet ##############
+##########################################################################
+
+
+help where
+cd .\Downloads
+$docs = dir
+dir
+$docs | where length -gt 20000
+$docs | where Name -eq "Arbeit"
+$docs | where Name -match "VS"
+$docs | where Name -cmatch "exe"
+$docs | where Name -like "*lat*"
+
+
+##########################################################################
+###################### 13. Filtering Using Loops #########################
+##########################################################################
+
+
+cd .\Downloads
+$docs = dir
+
+$docs | where {$_.length -gt 90000000}
+$docs | where {($_.length -gt 90000000) -and ($_.Name -cmatch "Ana")}
+
+
+
+
+##########################################################################
+######################### 14. Foreach Loop ###############################
+##########################################################################
+
+$var = 1..10
+
+$var1 = 1.10 | foreach {$_ *2}
+$var1
+
+
+$var2 = 1..10 |foreach {$_ *3}
+$var2
+
+$var3 = 1..5 | foreach {"This is a loop numer: $_ "}
+$var3
+
+
+$var4 = 1..5 | foreach {$_ *3} | foreach {"This is a loop numer: $_ "}
+$var4
+
+
+$var5 = "blah" * 4
+$var5
+
+$var6 = 1..10 | foreach {"*"*$_}
+$var6
+
+$var7 = 1..20 | foreach {if($_%2){"$_ is odd"}}
+$var7
+
+### Learning
+# {} - Curly Brackets
+# [] - Square Brackets
+# () - Brackets
+# "" - Quotation marks
+# ~ - Tilde
+# _ - underscore
+# | - Vertical Line
+### Learning
+
+
+
+
+
+##########################################################################
+############################ 15. Arrays ##################################
+##########################################################################
+
+
+$strComputers = @("Computer1", "Computer2", "Computer3", "Computer4")
+$strComputers
+$strComputers[0..1]
+
+$strComputers[3] = "Laptop4"
+$strComputers
+
+$strComputers + ("Laptop5")
+$strComputers_m = $strComputers + ("Laptop6")
+$strComputers_m
+
+
+$strComputers_m | gm
+$strComputers_m.Length
+$strComputers_m.Length
+$strComputers_m.Length
+
+
+$strComputers_m | foreach {if($_ -match "Lap"){"Found: $_"}}
+$strComputers_m | foreach {$_ + ": length is " + $_.length}
+
+
+
+
+##########################################################################
+############ 16. Hash Tables, Dictionory, HashMap ########################
+##########################################################################
+
+
+$HashTable = @{"Server1" = "100 mbt"; "Server2" = "300 mbt";}
+$HashTable
+
+$HashTable["Server1"] = "333 mbt"
+$HashTable
+
+
+$HashTable["BigData"] = "1000 mbt"
+$HashTable
+
+$HashTable.Remove("BigData")
+$HashTable
+
+
+
---
+++ 4. Formatting, Saving, and Importing Data.ps1
@@ -0,0 +1,120 @@
+﻿# 17. Formatting Output
+# 18. Saving Output
+# 19. Importing Data
+# 20. Practice Problem
+
+
+##########################################################################
+######################### 17. Formatting Output ##########################
+##########################################################################
+
+cd .\Downloads
+
+$files = dir
+$files | Format-Wide
+$files | Format-list
+$files | format-list -Property Name, Length, LastAccessTime
+$files | format-table -Property Name, Length, LastAccessTime
+$files | sort -Pr Length | format-table -Pr Name, Length, LastAccessTime
+
+
+Get-Process |format-table
+Get-Process |sort ProcessName | format-table
+
+Get-Process |format-table -Pr Path, Name, Id, Company
+
+Get-Process |format-table -Pr Path, Name, Id, Company -AutoSize
+Get-Process |sort -Pr Company|format-table -Pr Path, Name, Id -groupby Company
+
+
+
+
+##########################################################################
+######################### 18. Saving Output ##############################
+##########################################################################
+
+# Text File - TXT
+Get-Process | Out-File C:\Users\VadzimRamaniuk\Documents\Getprocess.txt
+dir C:\Users\VadzimRamaniuk\Documents\Getpr*.t*
+notepad C:\Users\VadzimRamaniuk\Documents\Getprocess.txt
+
+# HTML File - HTML
+Get-Process | ConvertTo-Html | Out-File C:\Users\VadzimRamaniuk\Documents\GetProcess.htm
+dir C:\Users\VadzimRamaniuk\Documents\GetPr*.h*
+Invoke-Expression C:\Users\VadzimRamaniuk\Documents\GetPr*.htm
+
+# Excel File - CSV
+Get-Process | Export-Csv C:\Users\VadzimRamaniuk\Documents\GP.csv
+dir C:\Users\VadzimRamaniuk\Documents\GP.csv
+Invoke-Expression C:\Users\VadzimRamaniuk\Documents\GP.csv
+
+
+
+##########################################################################
+######################### 19. Importing Data #############################
+##########################################################################
+
+# How to Open a Path with SPACE character.
+# Option 1: "&" - escape SPACE character
+"&'C:\Users\VadzimRamaniuk\Documents\Learning Path\PowerShell\Master Microsoft Powershell\Census1000.csv'" | Invoke-Expression
+
+
+# Option 2: The Grave Accent (`) - escape SPACE character.
+invoke-expression 'C:\Users\VadzimRamaniuk\Documents\Learning` Path\PowerShell\Master` Microsoft` Powershell\Census1000.csv'
+
+
+# option 3: Enclose the Path in Quotation Mark (") - escape SPACE Character.
+invoke-expression 'C:\Users\VadzimRamaniuk\Documents\"Learning Path"\PowerShell\"Master Microsoft Powershell"\Census1000.csv'
+
+# Option 2
+$names = Import-Csv C:\Users\VadzimRamaniuk\Documents\Learning` Path\PowerShell\Master` Microsoft` Powershell\Census1000.csv
+
+# Option 3
+$names = Import-Csv C:\Users\VadzimRamaniuk\Documents\"Learning Path"\PowerShell\"Master Microsoft Powershell"\Census1000.csv
+
+$names
+$names | format-table
+$names | gm
+
+
+
+##########################################################################
+######################### # 20. Practice Problem #########################
+##########################################################################
+
+# Which Surnaname has the Highest Percentage of People with that Surname that a Hispanic
+
+$names = Import-Csv C:\Users\VadzimRamaniuk\Documents\Learning` Path\PowerShell\Master` Microsoft` Powershell\Census1000.csv
+$names | Get-Member
+
+# Result 1:
+$names | sort -Property pcthispanic -Descending | select -Pr Name, pcthispanic -first 5 | format-table
+
+# Result 2:
+$names | sort -Pr pcthispanic -Descending | select -first 3 |format-table -Pr Name, pcthispanic
+
+# Result 3:
+($names | sort -Pr pcthispanic -Descending)[0..2]
+
+# Result 4:
+($names | sort -Pr pcthispanic -Descending)[0..2].name
+
+# Result 5:
+$names | sort -Property pcthispanic -Descending | select -Pr Name, pcthispanic -first 5 | format-table -Pr Name
+
+# Result 5:
+$names | foreach {if ($_.pcthispanic -gt $highest.pcthispanic){$highest=$_}}
+$highest
+$highest.name
+
+
+
+
+
+
+
+
+
+
+
+
